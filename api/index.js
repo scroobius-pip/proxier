bf552e85b2bf0cc976c508d8635cadd84279ef19
simdi jinkins <sim04ful@gmail.com>
2020-12-30 22:56:55 +0100

    Added cors

--- api/index.js
+++ api/index.js
@@ -38,7 +38,24 @@ const parseAnyReqBody = req =>
       .on('error', reject)
   })
 
-module.exports = async (req, res) => {
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = async (req, res) => {
   const { url, ...reqParams } = req.query
   if (!url) {
     return res.end('In order to use this proxy just append your desired url after the root `/`, feel free to use any method and headers.')
@@ -97,3 +114,5 @@ module.exports = async (req, res) => {
     })
 
 }
+
+module.exports = allowCors(handler)
