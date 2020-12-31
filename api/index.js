fa928d9f439ddc44703181f5dfc561b72e7c1fbb
scroobius-pip <sim04ful@gmail.com>
2020-12-31 10:44:32 +0100

    cors again

--- api/index.js
+++ api/index.js
@@ -42,7 +42,7 @@ const allowCors = fn => async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://fontofweb.com')
   // another common pattern
   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Expose-Headers', 'access-control-allow-origin')
+  res.setHeader('Access-Control-Expose-Headers', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
   res.setHeader(
     'Access-Control-Allow-Headers',
