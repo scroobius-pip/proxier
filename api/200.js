004d27b4b21305f4bb17831024fbaa4dbc40af92
scroobius-pip <sim04ful@gmail.com>
2020-12-31 10:57:35 +0100

    created allowCors.js

--- api/200.js
+++ api/200.js
@@ -1,22 +1,8 @@
+const allowCors = require('../lib/allowCors')
+
 const handler = async (req, res) => {
   res.status(200).send({ message: 'cors ok' });
 }
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://fontofweb.com')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Expose-Headers', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
 
 module.exports = allowCors(handler)
--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 const fetch = require('../lib/request')
+const allowCors = require('../lib/allowCors')
 
 function log(message, data) {
   console.log({
@@ -38,22 +39,6 @@ const parseAnyReqBody = req =>
       .on('error', reject)
   })
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://fontofweb.com')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Expose-Headers', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
 
 const handler = async (req, res) => {
   const { url, ...reqParams } = req.query
